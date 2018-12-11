15eccec3aeb6f03288eb24e4d7927713eb0ea695
cjuub <cjuuub@gmail.com>
2018-12-11 21:19:26 +0100

    Properly disable touchpad on dell xps 15.

--- dell_xps_15_9570/.toggle_touchpad.sh
+++ dell_xps_15_9570/.toggle_touchpad.sh
@@ -2,7 +2,17 @@
 
 if xinput list-props 12 | grep "Device Enabled (141).*1" >/dev/null
 then
-  xinput disable 12
+  #xinput disable 12
+  sudo modprobe -r hid_generic
+  sudo modprobe -r hid_multitouch
 else
-  xinput enable 12
+  #xinput enable 12
+  sudo modprobe hid_multitouch
+  sleep 1
+  # touchpad tap click enable
+  xinput set-prop 12 280 1
+  # touchpad speed
+  xinput set-prop 12 300 0.5
+  # touchpad natural scroll enable
+  xinput set-prop 12 288 1
 fi
